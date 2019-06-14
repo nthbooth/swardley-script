66969d674999d60e7e8a47d5daedf3e049ff32d2
Nick Booth <21123527+nthbooth@users.noreply.github.com>
2019-06-14 18:05:58 +0100

    added input box for JSON url

--- mapping/app.js
+++ mapping/app.js
@@ -12,17 +12,17 @@ router.use(function (req,res,next) {
 
 
 router.get('/', (req, res) => {
-	console.log(req.query.url)
+//	console.log(req.query.url)
+	if(req.query.url){
 	var stufftosend='<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" href="styles.css"></head><body>  <h2>    Scripting a Wardley Map  </h2>   <div id="wardley-map">  </div>  <script type="text/javascript" src="'.concat(req.query.url,'"></script>  <script type="text/javascript" src="/renderSwardley.js"></script></body></html>')
- 	res.send(stufftosend)
-	//res.send(req.query.url)	
-	//res.send('"></script>  <script type="text/javascript" src="/renderSwardley.js"></script></body></html>')
+	}
+	else
+	{
+	stufftosend="<html><head><link rel='stylesheet' type='text/css' href='styles.css'></head><body><h2>Enter URL For JSON Wardley Map</h2><form action='/' method='get' name='url'>URL:<input type='text' name='url'><input type='submit' value='submit'<</body></html>"
+	}
+	res.send(stufftosend)
 
 })
-
-//router.get('/', function(req,res){
-//	  res.sendFile(path + 'index.html');
-//});
 
 app.use(express.static(path));
 app.use('/', router);
