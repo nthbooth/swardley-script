14b172e217443b6bbb3ba895505448353a137e25
Nick <blocked>
2019-10-21 22:17:48 +0100

    more fixes

--- mapping/mapping.js
+++ mapping/mapping.js
@@ -22,11 +22,11 @@ router.get("/", (req, res) => {
 		title="Wardley Map";
 	}
 	if(req.query.url){
-	stufftosend='<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" href="styles.css"></head><body>  <h2>'+title+'</h2>   <div id="wardley-map">  </div>  <script type="text/javascript" src="'.concat(req.query.url,'"></script>  <script type="text/javascript" src="/renderSwardley.js"></script></body></html>');
+	stufftosend='<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"><head><link rel="stylesheet" type="text/css" href="styles.css"><title>Wardley Mapping</title></head><body>  <h2>'+title+'</h2>   <div id="wardley-map">  </div>  <script type="text/javascript" src="'.concat(req.query.url,'"></script>  <script type="text/javascript" src="/renderSwardley.js"></script></body></html>');
 	}
 	else
 	{
-	stufftosend="<html><head><link rel='stylesheet' type='text/css' href='styles.css'></head><body><h2>Enter URL For JSON Wardley Map</h2><form action='/' method='get' name='url'>URL:<input type='text' name='url'>Map Height:<input type='text' name='height'>Map Width:<input type='text' name='width'><input type='submit' value='submit'<</body></html>";
+	stufftosend="<html xmlns='http://www.w3.org/1999/xhtml' lang='en' xml:lang='en'><head><link rel='stylesheet' type='text/css' href='styles.css'><title>Wardley Mapping</title></head><body><h2>Enter URL For JSON Wardley Map</h2><form action='/' method='get' name='url'>URL:<input type='text' name='url'>Map Height:<input type='text' name='height'>Map Width:<input type='text' name='width'><input type='submit' value='submit'<</body></html>";
 	}
 	res.send(stufftosend);
 
